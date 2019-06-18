2db5fa1de867245adc0195c454a7ab4d701de5f7
oneils <lex.nox@gmail.com>
2019-06-18 13:11:12 +0300

    Added language parameter to hello function.

--- hello.go
+++ hello.go
@@ -3,15 +3,28 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
-		return englishHelloPrefix + "World"
+		name = "World"
 	}
-
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
 }
-func main() {
 
-	fmt.Println(Hello("world"))
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
 }
