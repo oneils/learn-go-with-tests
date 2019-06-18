c7ff12926f64bdf14a56872b4354c5b8fd7ea188
Alexey Bogdanov <lex.nox@gmail.com>
2019-06-18 23:10:13 +0300

    Refactored SumAllTails to correctly calculate sum of empty slices

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -7,6 +7,13 @@ import (
 
 func TestSum(t *testing.T) {
 
+	checkSumAllTails := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -22,9 +29,7 @@ func TestSum(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSumAllTails(t, got, want)
 
 	})
 
@@ -32,8 +37,13 @@ func TestSum(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSumAllTails(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSumAllTails(t, got, want)
 	})
 }
