223488959928d244e9ad29123fde895cc42645da
oneils <lex.nox@gmail.com>
2019-06-19 14:09:04 +0300

    Maps:     - implemented add functionality

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,10 +1,17 @@
 package maps
 
-import "errors"
+var (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 type Dictionary map[string]string
-
-var ErrNotFound = errors.New("could not find the word you were looking for")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -13,4 +20,19 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
