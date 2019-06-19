9fe6aaaea9ef086e78a9b3737e9ed00cf2a38684
oneils <lex.nox@gmail.com>
2019-06-19 16:08:09 +0300

    Mocking:     - added base functionality for Countdown

---
+++ mocking/coundown.go
@@ -0,0 +1,20 @@
+package mocking
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out *bytes.Buffer) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
+}
